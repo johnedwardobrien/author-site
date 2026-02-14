555997d3f8b218990822af92ecaa809c8f9cb9e7
John O'Brien <johnobrien8642@gmail.com>
2026-02-14 10:53:07 -0800

    add to render blocks

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -4,6 +4,7 @@ import type { Page } from '@/payload-types'
 
 import { AnimationBlock } from '@/blocks/AnimationBlock/Component'
 import { ArchiveBlock } from '@/blocks/ArchiveBlock/Component'
+import { BannerBlock } from '@/blocks/Banner/Component'
 import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { ContentBlock } from '@/blocks/Content/Component'
 import { FormBlock } from '@/blocks/Form/Component'
@@ -16,6 +17,7 @@ import { YachtParallax } from '@/blocks/YachtParallax/Component'
 const blockComponents = {
   animationBlock: AnimationBlock,
   archive: ArchiveBlock,
+  banner: BannerBlock,
   content: ContentBlock,
   cta: CallToActionBlock,
   formBlock: FormBlock,
