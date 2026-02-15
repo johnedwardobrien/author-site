e35861f52bb6f05d6e8a3107ccfff3651c149ab3
John O'Brien <johnobrien8642@gmail.com>
2026-02-15 09:54:35 -0800

    adjust responsive breakpoints for now

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -20,17 +20,21 @@ export const CollectionArchive: React.FC<Props> = (props) => {
       <div className="inner">
         <Slider
           className="collection-archive-swiper"
-          slidesToShow={5}
+          slidesToShow={4}
           slidesToScroll={1}
           arrows
           infinite={false}
           responsive={[
             {
               breakpoint: 1024,
-              settings: { slidesToShow: 3, slidesToScroll: 1 },
+              settings: { slidesToShow: 4, slidesToScroll: 1 },
             },
             {
               breakpoint: 768,
+              settings: { slidesToShow: 2, slidesToScroll: 1 },
+            },
+            {
+              breakpoint: 480,
               settings: { slidesToShow: 1, slidesToScroll: 1 },
             },
           ]}
