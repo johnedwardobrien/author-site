58a06103e93e69d95804bb86925b2e0ff493e047
John O'Brien <johnobrien8642@gmail.com>
2026-02-15 12:17:45 -0800

    switch back to swiper

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import React from 'react'
-import Slider from 'react-slick'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 import './Component.css'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 
 import { Card, CardPostData } from '@/components/Card'
 
@@ -17,61 +18,43 @@ export const CollectionArchive: React.FC<Props> = (props) => {
 
   return (
     <div className="collection-archive-cont">
-      <Slider
-        className="collection-archive-swiper"
-        slidesToShow={5}
-        slidesToScroll={2}
-        arrows
-        infinite={false}
-        responsive={[
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true,
-            },
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-        ]}
-      >
-        {posts?.map((result, index) => {
-          if (typeof result === 'object' && result !== null) {
-            return (
-              <Card key={index} className="h-full" doc={result} relationTo="posts" showCategories />
-            )
+      <div className="inner">
+        <Swiper
+          className="collection-archive-swiper"
+          slidesPerView={1}
+          spaceBetween={16}
+          navigation
+          modules={[Navigation]}
+          breakpoints={{
+            768: { slidesPerView: 3, spaceBetween: 24 },
+            1024: { slidesPerView: 4, spaceBetween: 24 },
+          }}
+        >
+          {posts?.map((result, index) => {
+            if (typeof result === 'object' && result !== null) {
+              return (
+                <SwiperSlide key={index}>
+                  <Card className="h-full" doc={result} relationTo="posts" showCategories />
+                </SwiperSlide>
+              )
+            }
+            return null
+          })}
+        </Swiper>
+        <style jsx global>{`
+          .collection-archive-cont .swiper-button-prev,
+          .collection-archive-cont .swiper-button-next {
+            display: none;
           }
-          return null
-        })}
-      </Slider>
-      <style jsx global>{`
-        .collection-archive-cont .slick-prev,
-        .collection-archive-cont .slick-next {
-          display: none;
-        }
 
-        @media (min-width: 768px) {
-          .collection-archive-cont .slick-prev,
-          .collection-archive-cont .slick-next {
-            display: flex;
+          @media (min-width: 768px) {
+            .collection-archive-cont .swiper-button-prev,
+            .collection-archive-cont .swiper-button-next {
+              display: flex;
+            }
           }
-        }
-      `}</style>
+        `}</style>
+      </div>
     </div>
   )
 }
